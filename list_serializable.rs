da6958bbd63c83970c6e94de0f6ccfbc4cce4a4d
Woody <WoodyY.Wu@outlook.com>
2024-08-01 11:53:34 -0400

    Add files via upload

---
+++ list_serializable.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct ListSerializable<T> {
+    accesses: Vec<T>,
+}
+
+impl<T> ListSerializable<T> {
+    pub fn new() -> Self {
+        ListSerializable {
+            accesses: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, value: T) {
+        self.accesses.push(value);
+    }
+
+    pub fn get_vec(&self) -> &Vec<T> {
+        &self.accesses
+    }
+}
+
+impl<T> Default for ListSerializable<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
